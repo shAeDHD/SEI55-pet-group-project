80040db5cacc680eda8bb7c409a59f0d1544024b
Mohamad Dakdouk <mdak2806@gmail.com>
2022-09-18 09:20:39 +1000

    update

--- frontend-pgp-react/src/components/Home.jsx
+++ frontend-pgp-react/src/components/Home.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Pet from './Pet';
+import User from './User';
+import Accessories from './Accessories';
 import { Route, HashRouter as Router, Link } from "react-router-dom";
+
+
 
 
 
@@ -11,10 +16,32 @@ class Home extends React.Component {
     render(){
 
         return (
+            <div className="App"> 
+            <Router>
+                <header>
+                    <h1>We shall decide</h1>
 
-            <div className="App"> SCWM</div>
+                    <nav>
+                        {/* Links to various pages */}
+                    <Link to="/">Home</Link>
+                    {''} | {''}
+                    <Link to="/pet">Pets</Link>
+                    {''} | {''}
+                    <Link to="/accessories">Accessories</Link>
+                    {''} | {''}
+                    <Link to="/users">Users</Link>
+                    </nav>
+
+
+                </header>
+                {/* Routes to the various pages */}
+                    <Route exact path="/users" component={User}/>
+                    <Route exact path="/accessories" component={Accessories}/>
+                    <Route exact path="/pet" component={Pet}/>
+            </Router>
+            </div>
  
-        ) // return
+        ); // return
 
     } // render
 
---
+++ frontend-pgp-react/src/components/User.jsx
@@ -0,0 +1,74 @@
+import axios from 'axios';
+import React from 'react';
+const RAILS_USER_BASE_URL = 'http://localhost:3000/users';
+
+
+
+class User extends React.Component{
+    state ={
+        users: [],
+        loading: true,
+        error: null
+    }
+    componentDidMount(){
+        // We want to load the Users data
+        console.log('componentDidMount for users()');
+        this.fetchUsers();
+    
+    } // Mount
+    fetchUsers = async () => {
+
+        try{
+            const res = await axios.get(RAILS_USER_BASE_URL);
+            console.log('users:', res.data)
+
+            this.setState({
+                users: res.data,
+                loading: false
+            });
+
+        } // try
+        catch( err ){
+            console.log('Error Loading secrets from API', err);
+
+            this.setState({
+                loading: false,
+                error: err // Store the required information for the render
+            });
+        }
+        
+    } // fetch
+
+    render(){
+
+        return (
+
+            <div className="Users">
+                <h1>Users</h1>
+                <ul>
+                {
+                this.state.users.map ( r => <li>
+                    Name: {r.name} <br /> 
+                    Display Name:{r.display_name} <br /> 
+                    User ID: {r.user_id} <br />
+                    <br />
+                </li>
+                )
+                }
+                </ul>
+            </div>
+
+        ) // return
+
+    } // render
+
+
+
+
+
+
+
+
+}
+
+export default User;
