8c4a407242aa26cd698879e399c5079ec57db393
Craig Rolleston <craig.rolleston@gmail.com>
2022-09-17 14:29:56 +1000

    routes resources

--- backend-PGP-server/config/routes.rb
+++ backend-PGP-server/config/routes.rb
@@ -1,24 +1,12 @@
 Rails.application.routes.draw do
-  get 'accessories/new'
-  get 'accessories/create'
-  get 'accessories/index'
-  get 'accessories/show'
-  get 'accessories/edit'
-  get 'accessories/update'
-  get 'accessories/destroy'
-  get 'pets/new'
-  get 'pets/create'
-  get 'pets/index'
-  get 'pets/show'
-  get 'pets/edit'
-  get 'pets/update'
-  get 'pets/destroy'
-  get 'users/new'
-  get 'users/create'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get '/users' => 'users#index'
+  
+
+
+  resources :users
+  resources :pets
+  resources :accessories
+
+
 end
