4f9bba5a584bffd47461127146d1cdcc8972c448
Craig Rolleston <craig.rolleston@gmail.com>
2022-09-18 10:34:39 +1000

    moves controller

--- backend-PGP-server/app/models/move.rb
+++ backend-PGP-server/app/models/move.rb
@@ -1,2 +1,5 @@
 class Move < ApplicationRecord
+
+    has_and_belongs_to_many :pets
+
 end
--- backend-PGP-server/config/routes.rb
+++ backend-PGP-server/config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
  
   post 'user_token' => 'user_token#create'
-# root to temporary home page
-root to: 'users#home'
+  # root to temporary home page
+  root to: 'users#home'
 
-# roots to login details
-get '/login' => 'sessions#new'
-post '/login' => 'sessions#create'
-delete '/login' => 'sessions#destroy' 
+  # roots to login details
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/login' => 'sessions#destroy' 
 
-#resources
-resources :users
-resources :pets
-resources :accessories
+  #resources
+  resources :users
+  resources :pets
+  resources :accessories
 
 
-end
+end 
