858f2b4bc1039c83543d61abb83b9d8951ed20c0
Craig Rolleston <craig.rolleston@gmail.com>
2022-09-18 15:50:20 +1000

    indenting

--- backend-PGP-server/config/routes.rb
+++ backend-PGP-server/config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
  
-# root to temporary home page
-root to: 'users#home'
+  # root to temporary home page
+  root to: 'users#home'
 
-# roots to login details
-get '/login' => 'sessions#new'
-post '/login' => 'sessions#create'
-delete '/login' => 'sessions#destroy' 
- #Get the login token from Knock
- post '/user_token' => 'user_token#create'
- #User routes
- get '/users/current' => 'users#current'
+  # roots to login details
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/login' => 'sessions#destroy' 
+  #Get the login token from Knock
+  post '/user_token' => 'user_token#create'
+  #User routes
+  get '/users/current' => 'users#current'
 
 
   get '/pet' => 'pets#new'
