cfaaa85447b37897b557beb2852dcc65a082efd2
Mohamad Dakdouk <mdak2806@gmail.com>
2022-09-19 22:12:54 +1000

    sum progress

--- frontend-pgp-react/src/components/CreateCritter.jsx
+++ frontend-pgp-react/src/components/CreateCritter.jsx
@@ -13,14 +13,27 @@ import owlet4frames from "../assets/owletMonster/fourFrames_owlet.png";
 import pink4frames from "../assets/pinkMonster/fourFrames_pink.png";
 import dude4frames from "../assets/dudeMonster/fourFrames_dude.png";
 
+// species2: 'dude4',
+// species3: 'owlet4',
+
 class CreateCritter extends React.Component{
     state = {
         currentUser: '',
         frame: '4',
         action: 'idle',
         species: 'pink4',
+        species2: 'dude4',
+        species3: 'owlet4',
+        classname: 'pink4',
+        clickCount: '',
         loading: true,
         error: null
+    }
+
+    renderSelection = {
+        'dude4': dude4,
+        'pink4': pink4,
+        'owlet4': owlet4,
     }
 
     render(){
@@ -30,7 +43,7 @@ class CreateCritter extends React.Component{
             <div className="Create">
                 <h1>Create your own Criiter</h1>
 
-                <div className="pink4FramesViewbox pixelArt" >
+                <div className={`${this.state.species}FramesViewbox pixelArt`} >
                 <img src={pink4frames} alt="character" 
                     className={`
                     idle
@@ -38,23 +51,25 @@ class CreateCritter extends React.Component{
                     pixelArt`}/>
                 </div>
                 <br />
-                <div className="dudeFourFramesViewbox pixelArt" >
+                {/* <div className="dude4FramesViewbox pixelArt" >
                     <img src={dude4frames} alt="character" 
-                    className="
+                    className={`
                     idle
-                    dudeFourFramesSpriteSheet
-                    pixelArt"/>
+                    ${this.state.species2}FramesSpriteSheet
+                    pixelArt`}/>
                 </div>
                 <br />
-                <div className="owletFourFramesViewbox pixelArt" >
+                <div className="owlet4FramesViewbox pixelArt" >
                     <img src={owlet4frames} alt="character" 
-                    className="
+                    className={`
                     idle
-                    owletFourFramesSpriteSheet
-                    pixelArt"/>
-                </div>
+                    ${this.state.species3}FramesSpriteSheet
+                    pixelArt`}/>
+                </div> */}
 
                 <br /><br />
+                <button className="left">Left</button>
+                <button className="right">Right</button>
                 <button className="select">Select Critter</button>
             </div>
 
