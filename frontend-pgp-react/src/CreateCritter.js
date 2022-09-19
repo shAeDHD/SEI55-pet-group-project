d621330ad1d97c6581b2c2bed326e0ccd76f1efc
Wen Gong <gongwen1201@gmail.com>
2022-09-19 18:39:45 +1000

    create CreateCritter.jsx

---
+++ frontend-pgp-react/src/CreateCritter.js
@@ -0,0 +1,5 @@
+import './CreateCritter.css';
+
+function CreateCritter() {
+    
+}
---
+++ frontend-pgp-react/src/components/CreateCritter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from 'axios'
+
+class CreateCritter extends React.Component{
+
+    render(){
+
+        return(
+            <div>
+
+            </div>
+        ) // return
+    } // render()
+} // class
+
+export default CreateCritter
